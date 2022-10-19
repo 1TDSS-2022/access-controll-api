3b117627fc38a5f6ebe7558ad15d905f64940b19
luisapurifica <rm95324@fiap.com.br>
2022-10-19 20:18:46 -0300

    sessionn

---
+++ rm95324/login/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function App() {
+
+  sessionStorage.setItem("nome", "Luisa")
+
+  return (
+    <div>
+      <h1>Controle de acesso</h1>
+    </div>
+  )
+}
+ 
