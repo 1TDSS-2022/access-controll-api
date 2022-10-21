8e9c09a35efd6223ae41a9ebcfab8239f3bd2a43
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-10-21 20:05:03 -0300

    Form Login até o Fetch response.

--- pf0670/login/src/components/Login.jsx
+++ pf0670/login/src/components/Login.jsx
@@ -1,5 +1,68 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [usuario, setUsuario] = useState({
+    login: "",
+    senha: "",
+  });
+
+  const handleChange = (e) => {
+    setUsuario({ ...usuario, [e.target.name]: e.target.value });
+  };
+
+  const logar = async (e) => {
+    e.preventDefault();
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(usuario),
+    };
+
+    const response = await fetch(
+      "http://localhost:8080/ControleAcesso/rest/login/",
+      requestOptions
+    );
+
+    const data = await response.json();
+  };
+
+  return (
+    <div>
+      <h2>LOGIN DE USUÁRIOS</h2>
+      <div>
+        <form onSubmit="">
+          <fieldset>
+            <legend>LOGIN</legend>
+            <div>
+              <label htmlFor="idLogin">Usuário</label>
+              <input
+                type="text"
+                name="login"
+                id="idLogin"
+                placeholder="Usuário"
+                value={usuario.login}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="idSenha">Senha</label>
+              <input
+                type="password"
+                name="senha"
+                id="idSenha"
+                placeholder="Senha"
+                value={usuario.senha}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <button>Logar</button>
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
 }
